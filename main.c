a6e81cb3b3b0bc1bff50f40ac30208cc14b66a92
ITejaswi <84660145+ITejaswi@users.noreply.github.com>
2021-07-01 18:26:29 +0530

    Update main.c

--- main.c
+++ main.c
@@ -26,35 +26,33 @@ int main(void) {
   test2();
   test3();
 }
-#include <stdio.h>
-#include <string.h>
-#include <cs50.h>
-#include <ctype.h>
-#include <math.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-string table[14] = {"Kindergarten", "First/Second Grade", "Third Grade", "Fourth Grade", "Fifth Grade", "Sixth Grade", "Seventh Grade",
-    "Eighth Grade", "Ninth Grade", "Tenth Grade", "Eleventh Grade", "Twelfth Grade", "College student", "Professor"};
+void ssort(int arr[],int l)
+{
+    int i;
 
-string ari(string s) {
-    int c = strlen(s);
-    int characters = 0;
-    int words = 0;
-    int sentences = 0;
-    for (int i=0;i<c;i++) {
-        if (isalnum(s[i])) {
-            characters +=1;
-        }
-        else if (s[i] == ' ') {
-            words +=1;
-        }
-        else if (s[i] == '.' || s[i] == '?' || s[i] == '!') {
-            sentences +=1;
-        }
+    for(i=0;i<l;i++)
+    {
+        swap_max(arr,l,i);
     }
-    float value = ((4.71*((float)characters/words)) + (0.5*((float)words/sentences)) - 21.43);
-    int a = (int)value;
-    int value2 = a +1;
-    int score = value2 - 1;
-    return table[score];
 }
 
+void swap_max(int arr[],int l,int n)
+{
+    int j,k;
+    int temp;
+
+    for(j=k=n;j<l;j++)
+    {
+        if(arr[j] > arr[k])
+        {
+            k = j;
+        }
+    }
+
+    temp = arr[n];
+    arr[n] = arr[k];
+    arr[k] = temp;
+}
